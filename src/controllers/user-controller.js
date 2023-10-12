4ba86fa9d9f6473946011307e11f40a2f205f3f3
Shiraz <shirazkk1@gmail.com>
2023-10-12 15:34:13 +0530

    Added client-error

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -37,11 +37,13 @@ const signIn = async (req, res) => {
                     });
           } catch (error) {
                     console.log(error);
-                    return res.status(500).json({
-                              message: 'Something went wrong',
+
+                    return res.status(error.statusCode).json({
+
+                              message: error.message,
                               data: {},
                               success: false,
-                              err: error
+                              err: error.explanation
                     });
           }
 }
@@ -65,25 +67,25 @@ const isAuthenticated = async (req, res) => {
                     });
           }
 }
-const isAdmin = async(req, res) => {
+const isAdmin = async (req, res) => {
           try {
-              const response = await userService.isAdmin(req.body.id);
-              return res.status(200).json({
-                  data: response,
-                  err: {},
-                  success: true,
-                  message: 'Successfully fetched whether user is admin or not'
-              })
+                    const response = await userService.isAdmin(req.body.id);
+                    return res.status(200).json({
+                              data: response,
+                              err: {},
+                              success: true,
+                              message: 'Successfully fetched whether user is admin or not'
+                    })
           } catch (error) {
-              console.log(error);
-              return res.status(500).json({
-                  message: 'Something went wrong',
-                  data: {},
-                  success: false,
-                  err: error
-              });
+                    console.log(error);
+                    return res.status(500).json({
+                              message: 'Something went wrong',
+                              data: {},
+                              success: false,
+                              err: error
+                    });
           }
-      }
+}
 module.exports = {
           create,
           signIn,
--- src/index.js
+++ src/index.js
@@ -13,19 +13,13 @@ const prepareAndStartServer = () => {
           app.use(bodyParser.urlencoded({ extended: true }));
 
           app.use('/api', apiRoutes);
-
+ 
           app.listen(PORT, async () => {
                     console.log(`Server Started on Port: ${PORT}`);
                     if (process.env.DB_SYNC) {
                               db.sequelize.sync({ alter: true });
                     }
 
-                    // const service = new UserService();
-                    // const newToken = service.createToken({email: 'sanket@admin.com', id: 1});
-                    // console.log("new token is", newToken);
-                    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MSwiaWF0IjoxNjcxNjM1MTgxLCJleHAiOjE2NzE2MzUyMTF9.vuo6QmYm6TmL2P2rBXoNDbPi1st5ZVrK4Yf2Jz-Dpzs';
-                    // const response = service.verifyToken(token);
-                    // console.log(response);
           });
 }
 prepareAndStartServer();
